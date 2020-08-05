005ffc5dd20355fc1fbb7f6cbd7f15b2cde849d9
Wilson Ruan <wilsonruan23@gmail.com>
2020-08-04 22:12:57 -0400

    Add comments

--- app.js
+++ app.js
@@ -14,19 +14,20 @@ const render = require("./lib/htmlRenderer");
 var employee = [];
 
 function inits() {
+  // Confirm user inputs number or something
   const confirmNumberValidator = async (input) => {
     return (isNaN(input) || input.length == 0) ? 'Please provide a valid number.': true;
   };
-
+  // Confirm user inputs something
   const confirmStringValidator = async (input) => {
     return (input.length == 0) ? 'Please provide a valid input.' : true;
   };
-
+// Confirm user inputs proper email
   const confirmEmailValidator = async (input) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return (!(re.test(String(input).toLowerCase()))) ? 'Please provide a valid email.' : true;
   };
-
+  // Generator Manager profile.
   function generatorManager() {
     inquirer.prompt([
         {
@@ -56,10 +57,13 @@ function inits() {
       ])
       .then((res) => {
         const manager = new Manager(res.name, res.id, res.email, res.officeNumber);
+        // Goes to Step 2: Give user options to choose and pushes the manager info to the next function.
         generatorOptions(manager);
       });
   }
+  //Provide choose to choose between ["Create Engineer Template", "Create Intern Template", "Start Your Team Template"],
   function generatorOptions(newEmployee) {
+    // Pushes information to var employee
     employee.push(newEmployee)
     inquirer.prompt([
         {
@@ -79,13 +83,17 @@ function inits() {
             generatorIntern();
             break;
             default:
+            // After users complete all profiles.  User can select here to create the team template.
+            // If not output test is available, the function inside if statement will create output test.
             if (!fs.existsSync(OUTPUT_DIR)) {
               fs.mkdirSync(OUTPUT_DIR);
             }
+            // Will create team.html and end the program here. 
             fs.writeFileSync(outputPath, render(employee), "utf-8");
         }
       });
   }
+  // Generator Enginee
   function generatorEngineer() {
     inquirer.prompt([
         {
@@ -115,9 +123,11 @@ function inits() {
       ])
       .then((res) => {
         const engineer = new Engineer(res.name, res.id, res.email, res.github);
+        // Loops back into options
         generatorOptions(engineer);
       });
   }
+  // Generator intern
   function generatorIntern() {
     inquirer.prompt([
         {
@@ -147,9 +157,12 @@ function inits() {
       ])
       .then((res) => {
         const intern = new Intern(res.name, res.id, res.email, res.school);
+        // Loops back into options
         generatorOptions(intern);
       });
   }
+  // Step 1: Generator Manager profile
   generatorManager();
 }
+// Start the function
 inits();
